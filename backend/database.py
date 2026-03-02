c1d6050c8954bd157edb3216fb379ff68debbd96
RYEONGRYEONG <josr428@naver.com>
2026-03-02 00:25:21 +0000

    debug: print db_url to track ssl_mode error

--- backend/database.py
+++ backend/database.py
@@ -4,14 +4,16 @@ from sqlalchemy import create_engine
 
 load_dotenv()
 
-def get_db():
-    db_user = os.getenv('db_user')
-    db_password = os.getenv('db_password')
-    db_host = os.getenv('db_host')
-    db_port = os.getenv('db_port')
-    db_name = os.getenv('db_name')
-    db_url = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
+db_user = os.getenv('db_user')
+db_password = os.getenv('db_password')
+db_host = os.getenv('db_host')
+db_port = os.getenv('db_port')
+db_name = os.getenv('db_name')
+db_url = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+
+def get_db():
     return create_engine(db_url)
 
 db_conn = get_db()
