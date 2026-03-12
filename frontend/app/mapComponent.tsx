4b0aa2f05d2260805b3352d4a05cf5acfc0ad98c
RYEONGRYEONG <josr428@naver.com>
2026-03-12 13:39:05 +0000

    feat: limit map bounds to Carlow

--- frontend/app/mapComponent.tsx
+++ frontend/app/mapComponent.tsx
@@ -5,6 +5,19 @@ import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import { useEffect } from 'react';
 
+const CARLOW_BOUND = {
+    minLat: 52.48,
+    maxLat: 52.92,
+    minLng: -7.15,
+    maxLng: -6.49
+};
+
+// Leaflet
+const bounds: L.LatLngBoundsExpression = [
+    [CARLOW_BOUND.minLat, CARLOW_BOUND.minLng],
+    [CARLOW_BOUND.maxLat, CARLOW_BOUND.maxLng]
+];
+
 const DefaultIcon = L.icon({
   iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
   shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
@@ -16,7 +29,10 @@ L.Marker.prototype.options.icon = DefaultIcon;
 function ClickHandler({ setPosition} : { setPosition: (pos: [number, number]) => void}){
     useMapEvents({
         click(e){
-            setPosition([e.latlng.lat, e.latlng.lng]);
+            const { lat, lng } = e.latlng;
+            if( lat >= CARLOW_BOUND.minLat && lat <= CARLOW_BOUND.maxLat &&
+                lng >= CARLOW_BOUND.minLng && lng <= CARLOW_BOUND.maxLng
+            ) setPosition([e.latlng.lat, e.latlng.lng]);
         },
     });
     return null;
@@ -30,7 +46,10 @@ interface MapModal {
 
 export default function MapContent({ position, setPosition, isSearch }: MapModal) {
   return (
-    <MapContainer center={position} zoom={13} style={{ height: '100%', width: '100%' }} zoomControl={false}>
+    <MapContainer center={position} zoom={13} style={{ height: '100%', width: '100%' }} zoomControl={false}
+    maxBounds = {bounds}
+    maxBoundsViscosity={1.0}
+    minZoom={10}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <ClickHandler setPosition={setPosition} />
       <Marker position={position} />
