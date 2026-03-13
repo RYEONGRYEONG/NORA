5bb20defa188dc39c8ff0c225945a5f829d67764
RYEONGRYEONG <josr428@naver.com>
2026-03-13 11:24:11 +0000

    fix: SQL syntax error

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,7 @@ app.add_middleware(
 def save_farm(farm: schema.FarmSave):
     try:
         with db_conn.connect() as conn:
-            query = text("insert into farms (farm_name, location_name, latitude, longitude, soil_condition) values (:name, :location, :lat, :lng, :soil")
+            query = text("insert into farms (farm_name, location_name, latitude, longitude, soil_condition) values (:name, :location, :lat, :lng, :soil)")
             conn.execute(query, {
                 "name": farm.farm_name,
                 "location": farm.location_name,
