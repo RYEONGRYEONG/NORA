1bdaf8b246fd495ff67fb91c4b99301056ef4567
RYEONGRYEONG <josr428@naver.com>
2026-03-23 04:36:27 +0000

    correct 2-day sliding window logic

--- backend/services/rain_service.py
+++ backend/services/rain_service.py
@@ -2,15 +2,9 @@ from datetime import timedelta
 from processors.rain_processor import calculate_rain_risk
 
 def is_heavy_rain(weather_list, soil_type, target_date):
-    if 'poorly' in soil_type:
-        days = 5
-    elif 'moderately' in soil_type:
-        days = 3
-    else:
-        days = 2
-        
+
     # calculate the total rainfall over the past days from "target date"
-    start = target_date - timedelta(days=days) # e.g, today: 17th
+    start = target_date - timedelta(days=2) # e.g, today: 17th
     end = target_date - timedelta(days=1) # end: 16th
 
     past_rain_sum = sum(item['rain'] for item in weather_list if start <= item['date'] <= end)
@@ -26,5 +20,4 @@ def is_heavy_rain(weather_list, soil_type, target_date):
         soil_type = soil_type,
         #target_date = target_date,
         past_rain_sum = past_rain_sum,
-        #days = days
     )
