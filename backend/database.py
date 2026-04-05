1abf3fcf2f0e74fdb509f9299b555e1d70d20087
RYEONGRYEONG <josr428@naver.com>
2026-04-05 20:19:25 +0100

    refactor: use single DATABASE_URL for better environment management

--- backend/database.py
+++ backend/database.py
@@ -4,13 +4,7 @@ from sqlalchemy import create_engine
 
 load_dotenv()
 
-
-db_user = os.getenv('db_user')
-db_password = os.getenv('db_password')
-db_host = os.getenv('db_host')
-db_port = os.getenv('db_port')
-db_name = os.getenv('db_name')
-db_url = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+db_url = os.getenv('DATABASE_URL')
 
 
 def get_db():
