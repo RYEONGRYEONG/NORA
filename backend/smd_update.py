be2f225bdbe2e6b2b57367ea95578e5420c35d25
RYEONGRYEONG <josr428@naver.com>
2026-04-05 17:11:25 +0100

    update: yesterday's weather data and SMD value

--- backend/smd_update.py
+++ backend/smd_update.py
@@ -1,7 +1,7 @@
 from services.smd_service import obs_analysis, save_results
 from database import db_conn 
 
-df = obs_analysis(25.41, 25.41, 22.73, db_conn) # 02/04
+df = obs_analysis(25.13, 25.13, 22.81, db_conn) # 04/04
 
 if df is not None:
     print(df.tail()) 
