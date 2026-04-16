54acd1d2e40c5ac414398edeb0bf97444f5809f5
Seongryeong Jo <126739223+RYEONGRYEONG@users.noreply.github.com>
2026-04-16 17:11:36 +0100

    test: add mock data for alert demo

--- backend/services/forecast_service.py
+++ backend/services/forecast_service.py
@@ -59,12 +59,12 @@ def update_farm_forecast(farm_id, lat, lon, conn, run_env='local'):
         })
 
 # Alert Demo Code
-    # demo = datetime.now()
-    # for val in db_data:
-    #    demo_time = datetime.strptime(val['time'], '%Y-%m-%d %H:%M:%S')
-    #     if demo_time > demo:
-    #         val['precip'] = 15.0
-    #         break
+    demo = datetime.now()
+    for val in db_data:
+        demo_time = datetime.strptime(val['time'], '%Y-%m-%d %H:%M:%S')
+        if demo_time > demo:
+            val['precip'] = 15.0
+            break
             
     try:
         query = text("""
